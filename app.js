ecbb12ad90ffce58988b02f727c4785127ce8aea
developergc <developergc@outlook.com>
2023-06-18 18:56:12 -0700

    a6

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
+import mongoose from "mongoose";
+
 const app = express()
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING)
 
 const allowedOrigins = ['https://a5--lambent-malasada-bfba84.netlify.app', 'http://localhost:3000'];
 app.use(cors({
--- users/auth-controller.js
+++ users/auth-controller.js
@@ -3,30 +3,44 @@ import * as usersDao from "./users-dao.js";
 
 const AuthController = (app) => {
   const register = async (req, res) => {
-    const username = req.body.username;
-    const user = await usersDao.findUserByUsername(username);
+    const user = await usersDao.findUserByUsername(req.body.username);
     if (user) {
-      res.sendStatus(409);
+      res.sendStatus(403);
       return;
     }
-    const tmp=req.body;
-    tmp._id = (new Date()).getTime() + '';
-    const newUser = usersDao.createUser(tmp);
-    req.session["currentUser"] = tmp;
-    res.json(tmp);
+    const newUser = await usersDao.createUser(req.body);
+    req.session["currentUser"] = newUser;
+    res.json(newUser);
   };
+
+  // const login = async (req, res) => {
+  //   const username = req.body.username;
+  //   const password = req.body.password;
+  //   const user = await usersDao.findUserByCredentials(username, password);
+  //   if (user) {
+  //     req.session["currentUser"] = user;
+  //     res.json(user);
+  //   } else {
+  //     res.sendStatus(404);
+  //   }
+  // };
 
   const login = async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const user = await usersDao.findUserByCredentials(username, password);
-    if (user) {
-      req.session["currentUser"] = user;
-      res.json(user);
+    if (username && password) {
+      const user = await usersDao.findUserByCredentials(username, password);
+      if (user) {
+        req.session["currentUser"] = user;
+        res.json(user);
+      } else {
+        res.sendStatus(403);
+      }
     } else {
-      res.sendStatus(404);
+      res.sendStatus(403);
     }
   };
+
 
   const profile =async (req, res) => {
     const currentUser = req.session["currentUser"];
@@ -42,11 +56,11 @@ const AuthController = (app) => {
     res.sendStatus(200);
   };
 
-  const update   = (req, res) => {
+  const update  = async (req, res) => {
 
     const userId = req.params['uid'];
     const updates = req.body;
-    usersDao.updateUser(userId,updates);
+    await usersDao.updateUser(userId,updates);
     res.json(updates);
     //res.sendStatus(200);
 
@@ -57,6 +71,6 @@ const AuthController = (app) => {
   app.post("/api/users/login",    login);
   app.post("/api/users/profile",  profile);
   app.post("/api/users/logout",   logout);
-  app.put ("/api/users/:uid",          update);
+  // app.put ("/api/users/:uid",     update);
 };
 export default AuthController;
